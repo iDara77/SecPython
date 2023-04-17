f2966020a823912a798a56b877ff20a0aadc227d
NH <nadim@ob-soft.com>
2023-04-17 10:55:58 +0000

    Added argument parser to TCP Server

--- recon-net/tcp_server.py
+++ recon-net/tcp_server.py
@@ -1,19 +1,42 @@
-import socket
+import socket, argparse, logging
 
-server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+     
+def argument_parser():
+    desc = "TCP server. Accepts host and port and returns hello before closing connection"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-o","--host",help="Host to bind on socket", default=socket.gethostname())
+    parser.add_argument("-p","--port",help="Port to bind on socket", default=444)
+    parser.add_argument("-l","--limit",help="Number of simultaneous connections",default=3)
+    parser.add_argument("-ll","--logging_level",help="Number of simultaneous connections",default=logging.INFO, type=int)
+    
+    user_args = vars(parser.parse_args())
+        
+    return user_args
 
-host = socket.gethostname()
-port = 444
+def main():
+    user_args = argument_parser()
+    FORMAT = '[%(asctime)s] %(message)s'
+    logging.basicConfig(level=user_args['logging_level'],format=FORMAT )
+    logging.getLogger().setLevel(level=user_args['logging_level'])
+    
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
-server.bind((host, port))
-server.listen(5)
+    host = user_args['host']
+    port = user_args['port']
+    limit = user_args['limit']
+    
+    server.bind((host, port))
+    server.listen(limit)
 
-print(f"Running TCP server on {host}:{port}")
-while True:
-    client, address = server.accept()
-    print(f"Received connection from {client} {address}")
-    message = "Hello!\r\n"
-    client.send(message.encode('ascii'))
-    print(f"Sent TCP to {client} {address}")
-    client.close()
-    print(f"Closed TCP with {client} {address}")
+    logging.info(f"Running TCP server on {host}:{port}")
+    while True:
+        client, address = server.accept()
+        logging.info(f"Received connection from {client} {address}")
+        message = "myTCP Server 1.0.0\r\n"
+        client.send(message.encode('ascii'))
+        logging.info(f"Sent TCP to {client} {address}")
+        client.close()
+        logging.info(f"Closed TCP with {client} {address}")
+    
+if __name__ == "__main__":
+    main()
