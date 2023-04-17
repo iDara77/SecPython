5773da529f6022345aa5b35ccc2b114def19225e
NH <nadim@ob-soft.com>
2023-04-17 10:58:58 +0000

    Added argument parser to UDP Server

--- recon-net/udp_server.py
+++ recon-net/udp_server.py
@@ -1,16 +1,37 @@
-import socket
+import socket, argparse, logging
 
-server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+     
+def argument_parser():
+    desc = "TCP server. Accepts host and port and returns hello before closing connection"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-o","--host",help="Host to bind on socket", default=socket.gethostname())
+    parser.add_argument("-p","--port",help="Port to bind on socket", default=666)
+    parser.add_argument("-ll","--logging_level",help="Number of simultaneous connections",default=logging.INFO, type=int)
+    
+    user_args = vars(parser.parse_args())
+        
+    return user_args
 
-host = socket.gethostname()
-port = 666
+def main():
+    user_args = argument_parser()
+    FORMAT = '[%(asctime)s] %(message)s'
+    logging.basicConfig(level=user_args['logging_level'],format=FORMAT )
+    logging.getLogger().setLevel(level=user_args['logging_level'])
 
-server.bind((host, port))
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-print(f"Running UDP server on {host}:{port}")
-while True:
-    message, address = server.recvfrom(1024)
-    print(f"Received UDP from {address} {message}")
-    message = "Hello!\r\n"
-    server.sendto(message.encode('ascii'),address)
-    print(f"Sent UDP {address}")
+    host = user_args['host']
+    port = user_args['port']
+
+    server.bind((host, port))
+
+    logging.info(f"Running UDP server on {host}:{port}")
+    while True:
+        message, address = server.recvfrom(1024)
+        logging.info(f"Received UDP from {address} {message}")
+        message = b'Hello!\r\n'
+        server.sendto(message,address)
+        logging.info(f"Sent UDP {address}")
+        
+if __name__ == "__main__":
+    main()
