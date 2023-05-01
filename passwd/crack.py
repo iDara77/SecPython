f36d272706de3f5c2c2ce3497161a00f7752cefd
NH <nadim@ob-soft.com>
2023-05-01 20:18:44 +0200

    Updated cracker to use dictionary

--- passwd/crack.py
+++ passwd/crack.py
@@ -1,8 +1,31 @@
 import crypt
+import os
 
-with open("./passwd/passwd") as f:
-    l = f.readline()[:-1]
-    hp = l.split(":")[1].strip()
-    hx = crypt.crypt("egg","HX")
-    if hp == hx:
-        print("password is 'egg'")
+dir = os.path.dirname(os.path.realpath(__file__))
+
+def testPass(passwd):
+    salt = passwd[0:2]
+    with open(f'{dir}/dict.txt') as f:
+        while True:
+            p = f.readline().strip("\n")
+            if not p:
+                break
+            c = crypt.crypt(p,salt)
+            if c == passwd:
+                print (f"[+] Found Password: {p}")
+                return
+    print ("[-] Password Not Found")
+
+def main():        
+    with open(f"{dir}/passwd") as f:
+        while True:
+            l = f.readline().strip("\n")
+            if not l:
+                break
+            l=l.split(":")
+            print (f"[*] Cracking Password For: {l[0]}")
+            hp = l[1].strip()
+            testPass(hp)
+            
+if __name__ == "__main__": 
+    main()
