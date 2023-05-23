2ffc8d6dfc4a01764f2ff04c6da3ce5a9f1d5538
NH <nadim@ob-soft.com>
2023-05-23 14:51:59 +0000

    Added code loader

--- obfuscation/codeobfuscator.py
+++ obfuscation/codeobfuscator.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import os,math
+import zlib
 
-## Ouvrir le fichier image et extraire la taille pour calculer la taille maximale du code
 DIR = os.path.dirname(os.path.realpath(__file__))
 IMAGE_ORIG=f"{DIR}/image_orig.jpg"
+CODE_ORIG=f"{DIR}/dataobfuscator.py"
 
+## Ouvrir le fichier image et extraire la taille pour calculer la taille maximale du code
 def loadImage(path):
     with Image.open(path) as img:
         img.load()
@@ -14,5 +16,25 @@ img = loadImage(IMAGE_ORIG)
 print(f"Image size: {img.size}")
 max_code_size = math.floor(img.size[0]*img.size[1]/8)
 print(f"Maximum code length: {max_code_size}")
+################
+
+## Ouvrir le fichier de code, le lire, et le compresser
+def readCode(path):
+    with open(path, "rb") as f:
+        code = f.read()
+    return code
+
+def compress(string):
+    compressed = zlib.compress(string)
+    return compressed
+
+def loadCode(path):
+    code = readCode(path)
+    print(f"Original code length: {len(code)}")
+    code = compress(code)
+    print(f"Compressed code length: {len(code)}")
+    return code
+
+code = loadCode(CODE_ORIG)
 ################
 
