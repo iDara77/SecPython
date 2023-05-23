300633cee9996fc62627d337aa044d6f09426338
NH <nadim@ob-soft.com>
2023-05-23 14:59:52 +0000

    Added obfuscation code

--- obfuscation/codeobfuscator.py
+++ obfuscation/codeobfuscator.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import os,math
 import zlib
+from bitstring import BitArray
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 IMAGE_ORIG=f"{DIR}/image_orig.jpg"
 CODE_ORIG=f"{DIR}/dataobfuscator.py"
+LSB_PAYLOAD_LENGTH_BITS = 32
 
 ## Ouvrir le fichier image et extraire la taille pour calculer la taille maximale du code
 def loadImage(path):
@@ -36,5 +38,41 @@ def loadCode(path):
     return code
 
 code = loadCode(CODE_ORIG)
+################
+
+## Valider que la taille du code compressé est inférieure à la tailla maximale permise
+if len(code)*8 > max_code_size:
+    raise Exception("Code size cannot be greather than image size")
+################
+
+## Utiliser le code d’obfuscation pour mettre le code dans l’image
+def obfuscate_via_lsb(data, img):
+    data = BitArray(uint=len(data) * 8, length=LSB_PAYLOAD_LENGTH_BITS).bin + BitArray(bytes=data).bin
+
+    i = 0
+    try:
+        width, height = img.size
+        if len(data) > width * height * 3:
+            print("Data is too large to be embedded in the image. Data contains {} bytes, maximum is {}".format(
+                int(len(data) / 8), int(width * height * 3 / 8)))
+            exit(1)
+        for x in range(0, width):
+            for y in range(0, height):
+                pixel = list(img.getpixel((x, y)))
+                for n in range(0, 3):
+                    if i < len(data):
+                        pixel[n] = pixel[n] & ~1 | int(data[i])
+                        i += 1
+                img.putpixel((x, y), tuple(pixel))
+                if i >= len(data):
+                    break
+            if i >= len(data):
+                break
+        return img
+    except IOError:
+        print("Could not open {}. Check that the file exists and it is a valid image file.".format(input_file))
+        exit(1)
+
+obfuscate_via_lsb(code, img)
 ################
 
